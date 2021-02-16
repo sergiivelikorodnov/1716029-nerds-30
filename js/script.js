07fc4024dd9c8e88d6ca197a5f1868262bbeec05
sergiivelikorodnov <sergii.velikorodnov@gmail.com>
2021-02-16 21:06:23 +0100

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var element = document.getElementById("#popup");
+
+    function showPopup() {
+      element.classList.toggle("show-popup");
+    }
+
+    function hidePopup() {
+      element.classList.toggle("hide-popup");
+    }
