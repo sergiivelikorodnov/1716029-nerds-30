7de0ad9b62a2495765e8e873d657b07c44c52b16
sergiivelikorodnov <77693268+sergiivelikorodnov@users.noreply.github.com>
2021-03-04 15:56:47 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,14 +5,14 @@ let formContact = popup.querySelector('.contact-form');
 let formOutput = popup.querySelector('.form-output');
 let formName = popup.querySelector('.form-name');
 let formEmail = popup.querySelector('.form-email');
-let formMessage = popup.querySelector('.form-message');
+let formMessage = popup.querySelector('.form-message .form-message-field');
 
 openPopupButton.addEventListener('click', function (evt) {
   evt.preventDefault();
   popup.classList.remove('hide-popup');
   popup.classList.add('show-popup');
   formName.focus();
-  });
+});
 
 closePopupButton.addEventListener('click', function (evt) {
   evt.preventDefault();
@@ -23,24 +23,38 @@ closePopupButton.addEventListener('click', function (evt) {
 
 document.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
-    if ( popup.classList.contains("show-popup")) {
-    popup.classList.remove('show-popup');
-    popup.classList.add('hide-popup');
+    if (popup.classList.contains('show-popup')) {
+      popup.classList.remove('show-popup');
+      popup.classList.add('hide-popup');
+    }
   }
-}
 });
 
- formContact.addEventListener("submit", function (evt) {
-  if (formName.value && formEmail.value && formMessage) {
+formContact.addEventListener("submit", function (evt) {
+  if (!formName.value || !formEmail.value || !formMessage) {
+    formName.classList.remove('invalid-value');
+    formEmail.classList.remove('invalid-value');
+    formMessage.classList.remove('invalid-value');
+    evt.preventDefault();
+    if (formName.value == '') {
+      formName.classList.add('invalid-value');
+    }
+    if (formEmail.value == '') {
+      formEmail.classList.add('invalid-value');
+    }
+    if (formMessage.value == '') {
+      formMessage.classList.add('invalid-value');
+    }
+  } else {
     evt.preventDefault();
     popup.classList.remove('show-popup');
     popup.classList.add('hide-popup');
-    console.log('Сообщение отправлено');
-    formName.value='';
-    formEmail.value='';
-    formMessage.value='';
-  }else{
-    evt.preventDefault();
-      }
+    formName.value = '';
+    formEmail.value = '';
+    formMessage.value = '';
+    formName.classList.remove('invalid-value');
+    formEmail.classList.remove('invalid-value');
+    formMessage.classList.remove('invalid-value');
+  }
 });
 
